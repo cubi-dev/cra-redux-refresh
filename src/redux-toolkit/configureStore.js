9b9d2ba0bea9d9ea1a337d85de37d1f0ca7787ec
cubi-dev <cubiiwork@gmail.com>
2024-05-05 15:11:28 +0700

    Update: Example about Redux Middleware

--- src/redux-toolkit/configureStore.js
+++ src/redux-toolkit/configureStore.js
@@ -1,13 +1,39 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import counterSlice from "./counterSlice";
+import counterSlice, { incrementByValue } from "./counterSlice";
+import { logger } from "redux-logger";
 import globalSlice from "./globalSlice";
 
 const reducer = combineReducers({
   counter: counterSlice,
-  global: globalSlice, 
+  global: globalSlice,
 });
+// My custom middleware to log the state
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   // your code here
+//   console.log(action);
+//   next(action);
+// };
+
+// const loggerMiddleware= function (store) {
+//   return function (next) {
+//     return function (action) {
+//       // your code here
+//     };
+//   }
+// }
 
 const store = configureStore({
   reducer: reducer,
+  middleware: (gDM) => gDM().concat(logger),
 });
+
+// store.subscribe(() => {
+//   // javascript observer pattern
+//   // console.log(`current state: ${store.getState().counter.count}`);
+// });
+
+store.dispatch(incrementByValue(1));
+// store.dispatch(incrementByValue(3));
+// store.dispatch(incrementByValue(5));
+
 export default store;
